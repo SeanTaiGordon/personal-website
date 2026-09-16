f057ca94ccd10e82675f28ee536782197c088540
SeanTaiGordon <sean@seangordon.co.uk>
2026-09-16 10:43:02 +0100

    Add prettier and ESLint (AirBnB)

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,71 @@
+import path from "node:path";
+import { fileURLToPath } from "node:url";
 import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import { FlatCompat } from "@eslint/eslintrc";
+import nextPlugin from "@next/eslint-plugin-next";
+import prettier from "eslint-config-prettier/flat";
+import { configs as tsConfigs } from "typescript-eslint";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
 
 const eslintConfig = defineConfig([
-  ...nextVitals,
-  ...nextTs,
-  // Override default ignores of eslint-config-next.
+  ...compat.extends("airbnb", "airbnb/hooks"),
+  ...tsConfigs.recommended,
+  {
+    ...nextPlugin.configs["core-web-vitals"],
+    files: ["**/*.{js,jsx,mjs,ts,tsx}"],
+  },
+  {
+    files: ["**/*.{js,jsx,mjs,ts,tsx}"],
+    settings: {
+      "import/resolver": {
+        typescript: true,
+        node: true,
+      },
+    },
+    rules: {
+      "react/react-in-jsx-scope": "off",
+      "react/jsx-filename-extension": [
+        "error",
+        { extensions: [".jsx", ".tsx"] },
+      ],
+      "import/prefer-default-export": "off",
+      "import/extensions": [
+        "error",
+        "ignorePackages",
+        {
+          js: "never",
+          jsx: "never",
+          mjs: "never",
+          ts: "never",
+          tsx: "never",
+        },
+      ],
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        { argsIgnorePattern: "^_", varsIgnorePattern: "^_" },
+      ],
+    },
+  },
+  {
+    files: ["eslint.config.*"],
+    rules: {
+      "import/no-extraneous-dependencies": "off",
+      "no-underscore-dangle": "off",
+    },
+  },
+  prettier,
   globalIgnores([
-    // Default ignores of eslint-config-next:
     ".next/**",
     "out/**",
     "build/**",
+    "node_modules/**",
+    ".vscode/**",
     "next-env.d.ts",
   ]),
 ]);
